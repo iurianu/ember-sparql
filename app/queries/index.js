fe5442f3952cbf7885afe4c6884a5602b3a52b75
Iulian Andriescu <iulianandriescu@gmail.com>
2022-04-23 13:57:11 +0300

    change ns, and property name

--- app/queries/index.js
+++ app/queries/index.js
@@ -1,16 +1,16 @@
 export const query = `
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX el: <http://data.europa.eu/eli/ontology#>
+PREFIX eli: <http://data.europa.eu/eli/ontology#>
 PREFIX aw: <https://data.vlaanderen.be/id/concept/AardWetgeving/>
 
 SELECT DISTINCT * WHERE {
-  ?article el:type_document aw:Decreet;
-          el:is_realized_by ?published.
+  ?article eli:type_document aw:Decreet;
+          eli:is_realized_by ?published.
 
-  ?published el:date_publication ?date;
-                   el:id_local ?itemid;
-                       el:title ?title.
+  ?published eli:date_publication ?date;
+                   eli:id_local ?itemid;
+                       eli:title ?title.
  }
 ORDER BY DESC(?date)
 LIMIT 5`
